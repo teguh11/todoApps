1ea48b84aa82d0ae1424a2bdd5fe145375bd6eda
teguh kristianto <teguh.kristianto@activymobility.com>
2018-12-22 16:05:02 +0700

    readme.md

---
+++ database/migrations/2018_12_22_084936_create_todo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('todo', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('todo');
+    }
+}
---
+++ redme.md
@@ -0,0 +1,12 @@
+# HOW TO USE
+
+## Database
+1. Create database `todo_apps`.
+2. run php artisan migrate
+
+## Running
+1. run composer install
+2. open in browser http://localhost/todoapps/public
+3. enjoy it!
+
+##POWERED BY LARAVEL FRAMEWORK
